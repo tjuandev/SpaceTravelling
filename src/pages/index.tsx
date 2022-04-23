e34297d9428257e638210805f064a6bc8b9c84e4
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-04-23 12:37:29 -0300

    First renderization of posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { GetStaticProps } from 'next';
-
+import * as PrismicH from '@prismicio/helpers';
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
+/* import commonStyles from '../styles/common.module.scss';
+import styles from './home.module.scss'; */
 
 interface Post {
   uid?: string;
@@ -24,13 +24,56 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): React.ReactElement {
-  return <div>oii</div>;
+export default function Home({
+  postsPagination,
+}: HomeProps): React.ReactElement {
+  const { results } = postsPagination;
+
+  return (
+    <>
+      {results.map(post => {
+        return (
+          <article key={post.uid}>
+            <h2>{post.data.title}</h2>
+            <p>{post.data.subtitle}</p>
+
+            <div>
+              <time>{post.first_publication_date}</time>
+              {post.data.author}
+            </div>
+            <button type="button">oi</button>
+          </article>
+        );
+      })}
+    </>
+  );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  /* eslint-disable */
+  const postsResponse = await prismic.getByType('post', {
+    pageSize: 5,
+  });
+  /* eslint-enable */
 
-//   // TODO
-// };
+  const posts: Post[] = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: PrismicH.asText(post.data?.title),
+        subtitle: PrismicH.asText(post.data?.subtitle),
+        author: post.data?.author,
+      },
+    };
+  });
+
+  const nextPageUrl = `${postsResponse.next_page}&access_token=${process.env.NEXT_PUBLIC_PRISMIC_ACCESS_TOKEN}`;
+
+  return {
+    props: {
+      postsPagination: { results: posts, next_page: nextPageUrl },
+    },
+  };
+};

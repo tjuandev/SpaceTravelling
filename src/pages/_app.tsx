a31c809aa4748c8e5803d43d7e5f60a02af966ee
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-04-24 10:19:33 -0300

    Using header component in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import Header from 'components/Header';
 import { AppProps } from 'next/app';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;

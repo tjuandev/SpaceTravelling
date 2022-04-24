395eb196b7fe4935f2568174a99c3e773a549e02
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-04-24 09:45:41 -0300

    Styling and creating Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
-export default function Header() {
-  // TODO
+import logo from 'assets/Logo.png';
+import Image from 'next/image';
+import styles from './header.module.scss';
+
+export default function Header(): React.ReactElement {
+  return (
+    <header className={styles.container}>
+      <Image src={logo} alt="logo" />
+    </header>
+  );
 }

df9393ff5e97b868eb32718c787c13da7b0fbc39
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-04-24 12:28:19 -0300

    Using common styles in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import logo from 'assets/Logo.png';
 import Image from 'next/image';
+import commonStyles from 'styles/common.module.scss';
 import styles from './header.module.scss';
 
 export default function Header(): React.ReactElement {
   return (
-    <header className={styles.container}>
+    <header className={`${styles.container} ${commonStyles.alignMaxWidth}`}>
       <Image src={logo} alt="logo" />
     </header>
   );

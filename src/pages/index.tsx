71161a871e2858280ff3815d31cbf8527e4ef7d0
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-04-24 09:42:11 -0300

    Making all logic of home page fetch and pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { GetStaticProps } from 'next';
+
 import * as PrismicH from '@prismicio/helpers';
+import { Query, PrismicDocument } from '@prismicio/types';
+import { useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
 
 /* import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss'; */
-
 interface Post {
   uid?: string;
   first_publication_date: string | null;
@@ -15,6 +17,9 @@ interface Post {
   };
 }
 
+type AdaptPrismicResponse = (post: Query<PrismicDocument>) => Post[];
+type GetNextPage = (route: string) => Promise<Query<PrismicDocument>>;
+
 interface PostPagination {
   next_page: string;
   results: Post[];
@@ -24,40 +29,8 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home({
-  postsPagination,
-}: HomeProps): React.ReactElement {
-  const { results } = postsPagination;
-
-  return (
-    <>
-      {results.map(post => {
-        return (
-          <article key={post.uid}>
-            <h2>{post.data.title}</h2>
-            <p>{post.data.subtitle}</p>
-
-            <div>
-              <time>{post.first_publication_date}</time>
-              {post.data.author}
-            </div>
-            <button type="button">oi</button>
-          </article>
-        );
-      })}
-    </>
-  );
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient({});
-  /* eslint-disable */
-  const postsResponse = await prismic.getByType('post', {
-    pageSize: 5,
-  });
-  /* eslint-enable */
-
-  const posts: Post[] = postsResponse.results.map(post => {
+const adaptPrismicResponse: AdaptPrismicResponse = ({ results: posts }) => {
+  return posts.map(post => {
     return {
       uid: post.uid,
       first_publication_date: post.first_publication_date,
@@ -68,6 +41,68 @@ export const getStaticProps: GetStaticProps = async () => {
       },
     };
   });
+};
+
+export default function Home({
+  postsPagination,
+}: HomeProps): React.ReactElement {
+  const { results, next_page } = postsPagination;
+
+  const [nextPage, setNextPage] = useState(next_page);
+  const [posts, setPosts] = useState(results);
+
+  const getNextPage: GetNextPage = async route => {
+    try {
+      const response = await fetch(route);
+      const newPosts = await response.json();
+
+      setNextPage(newPosts.next_page);
+
+      return setPosts(prevState => [
+        ...prevState,
+        ...adaptPrismicResponse(newPosts),
+      ]);
+    } catch (e) {
+      return e.message;
+    }
+  };
+
+  return (
+    <>
+      {posts.map(post => {
+        return (
+          <article key={post.uid}>
+            <h2>{post.data.title}</h2>
+            <p>{post.data.subtitle}</p>
+
+            <div>
+              <time>{post.first_publication_date}</time>
+              {post.data.author}
+            </div>
+          </article>
+        );
+      })}
+      <button
+        disabled={!nextPage}
+        onClick={() => getNextPage(next_page)}
+        type="button"
+      >
+        oi
+      </button>
+    </>
+  );
+}
+
+/* eslint-disable testing-library/no-await-sync-query */
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+
+  const postsResponse = await prismic.getByType('post', {
+    pageSize: 5,
+  });
+
+  const posts = adaptPrismicResponse(postsResponse);
 
   const nextPageUrl = `${postsResponse.next_page}&access_token=${process.env.NEXT_PUBLIC_PRISMIC_ACCESS_TOKEN}`;
 

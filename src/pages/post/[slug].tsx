e40b09cf8c6af4f87de74f7414cb3129a63f3e85
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-04-25 20:07:32 -0300

    Creating and styling staticProps posts without banner

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,45 +1,79 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
+import { PrismicRichText } from '@prismicio/react';
+import * as PrismicH from '@prismicio/helpers';
+import Metadata from 'components/Metadata';
+import commonStyles from 'styles/common.module.scss';
 import { getPrismicClient } from '../../services/prismic';
 
-import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
-  first_publication_date: string | null;
+  first_publication_date: string;
+  title: string;
   data: {
-    title: string;
-    banner: {
-      url: string;
-    };
     author: string;
-    content: {
-      heading: string;
-      body: {
-        text: string;
-      }[];
-    }[];
   };
+  estimatedReadingTime: string;
+  content: [];
 }
 
 interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps): React.ReactElement {
+  return (
+    <section className={commonStyles.alignMaxWidth}>
+      <article className={styles.container}>
+        <h1>{post.title}</h1>
+        <Metadata post={post} />
+        <PrismicRichText field={post.content} />
+      </article>
+    </section>
+  );
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+/* eslint-disable testing-library/no-await-sync-query */
 
-//   // TODO
-// };
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient({});
+  const posts = await prismic.getByType('post');
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const prismic = getPrismicClient({});
+  const { data, first_publication_date } = await prismic.getByUID(
+    'post',
+    String(params.slug)
+  );
+
+  const allTextsFromContent = data.content.reduce(
+    (acc, crr) => acc?.text || acc + crr.text
+  );
+
+  const estimatedReadingTime = `${Math.round(
+    allTextsFromContent.split('').length / 200
+  )} Minutes`;
+
+  const post: Post = {
+    first_publication_date,
+    title: PrismicH.asText(data.title),
+    data: {
+      author: data.author,
+    },
+    content: data.content,
+    estimatedReadingTime,
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};

561dd1cd99690b3e7977583ebecb6ab04cb82641
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-04-28 19:30:24 -0300

    Adding link property to Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import logo from 'assets/Logo.png';
 import Image from 'next/image';
+import Link from 'next/link';
 import commonStyles from 'styles/common.module.scss';
 import styles from './header.module.scss';
 
 export default function Header(): React.ReactElement {
   return (
     <header className={`${styles.container} ${commonStyles.alignMaxWidth}`}>
-      <Image src={logo} alt="logo" />
+      <Link as="/" href="/">
+        <Image src={logo} alt="logo" />
+      </Link>
     </header>
   );
 }
